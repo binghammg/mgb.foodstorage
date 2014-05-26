9f89f79e20d2d146b9192d2ce14fea7b0f953402
Mike Bingham <binghammg@gmail.com>
2014-05-25 22:21:51 -0600

    Initial commit

---
+++ mgb.foodstorage/FoodStorage.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace mgb.foodstorage
+{    
+    public class FoodStorage : IFoodStorage
+    {
+        public void AddItem()
+        {
+
+        }
+
+        public void DeleteItem()
+        {
+
+        }
+
+        public void UpdateItem()
+        {
+
+        }
+
+        public void AddThreshold()
+        {
+
+        }
+
+        public void DeleteThreshold()
+        {
+
+        }
+
+        public void UpdateThreshold()
+        {
+
+        }
+
+        public void GetAllItems()
+        {
+
+        }
+
+        public void GetItem()
+        {
+
+        }
+    }
+}
---
+++ mgb.foodstorage/IFoodStorage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace mgb.foodstorage
+{
+    [ServiceContract]
+    public interface IFoodStorage
+    {
+                
+        [OperationContract]
+        void AddItem();
+        
+        [OperationContract]
+        void DeleteItem();
+        
+        [OperationContract]
+        void UpdateItem();
+
+        [OperationContract]
+        void AddThreshold();
+
+        [OperationContract]
+        void DeleteThreshold();
+
+        [OperationContract]
+        void UpdateThreshold();
+
+        [OperationContract]
+        void GetAllItems();
+
+        [OperationContract]
+        void GetItem();
+    }    
+}
